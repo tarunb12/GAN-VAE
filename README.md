98d944830c270d968d45bb7bf07f6db90ee137c4
Tarun Belani <tbelani@ufl.edu>
2020-09-30 22:36:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GAN VAE - MNIST
---
+++ train/discriminator.py
@@ -0,0 +1,11 @@
+# %% Run from root
+import os
+if os.getcwd() == os.path.dirname(os.path.abspath(__file__)):
+    os.chdir('..')
+
+# %% Import ML libraries
+# pylint: disable=E0401
+from config import ROOT_DIR
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
